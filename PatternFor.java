f7e18bac91cd9d1b0ba0d146963a3322fb3f88e9
pramodhkesavSoftwareDev <pramodhkesav758@gmail.com>
2021-11-11 14:52:56 +0530

    Pattern program in Nested Forloop  Java

---
+++ PatternFor.java
@@ -0,0 +1,13 @@
+public class PatternFor {
+ 
+    public static void main(String[] args) {
+        for(int end=5;end>=1;end--)
+        {
+            for(int val=1;val<=end;val++) {
+                System.out.print(val+" ");
+            }
+            System.out.println();
+             
+        }
+    }
+    }
