a1182460abdef945a28d53d4b0970249a6ded807
pramodhkesavSoftwareDev <pramodhkesav758@gmail.com>
2021-11-13 23:31:00 +0530

    Alphabhet pattern

---
+++ Alphabhet.java
@@ -0,0 +1,27 @@
+class Alphabhet{
+public static void main(String[] args){
+for(int i=1;i<=7; i++)
+    {
+      System.out.print("* ");
+    }
+    System.out.println();
+    for(int line=1; line<=7;line++)
+    {
+      /*
+       * for(int at=1; at<=3;at++) { System.out.print("@ "); }
+       */
+    System.out.print("* ");
+    if(line==4)
+    {
+      System.out.print("* ");
+      System.out.print("* ");
+      System.out.print("* ");
+    }
+    System.out.println();
+    }
+    for(int i=1;i<=7; i++)
+    {
+      System.out.print("* ");
+    }
+}
+}
