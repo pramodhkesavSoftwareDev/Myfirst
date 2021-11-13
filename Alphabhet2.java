1a5d3577f7dd95cb5e04bd7f46c4edf2296e8f60
pramodhkesavSoftwareDev <pramodhkesav758@gmail.com>
2021-11-13 23:32:51 +0530

    Alphabhet pattern2

---
+++ Alphabhet2.java
@@ -0,0 +1,18 @@
+class Alphabhet2{
+public static void main(String[] args){
+for(int row =1; row<=9;row++)
+    {
+      if(row==5)
+      {
+      for(int star=1;star<=7;star++)
+        {
+      System.out.print("* ");
+        }
+      }
+      else{
+          System.out.print("*           *");
+      }
+    System.out.println();
+    }
+}
+}
