03d0e5b2b63a560ac8eafb30ea7a51df1b525a15
pramodhkesavSoftwareDev <pramodhkesav758@gmail.com>
2021-11-17 00:06:59 +0530

    Alphabhet patterns

---
+++ PatternI.java
@@ -0,0 +1,22 @@
+class PatternI {
+    public static void main(String[] args) {
+        for(int i=1;i<=7; i++)
+    {
+      System.out.print("* ");
+    }
+    System.out.println();
+    for(int line=1; line<=7;line++)
+    {
+      
+      for(int at=1; at<=3;at++) { System.out.print("  "); }
+       
+    System.out.print("* ");
+   
+    System.out.println();
+    }
+    for(int i=1;i<=7; i++)
+    {
+      System.out.print("* ");
+    }
+    }
+}
