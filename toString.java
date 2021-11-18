c0d624d45766f00fbebd7bac4bab753b2895bbf3
pramodhkesavSoftwareDev <pramodhkesav758@gmail.com>
2021-11-18 14:04:49 +0530

    Array learning

---
+++ toString.java
@@ -0,0 +1,11 @@
+public class toString {
+ 
+    public static void main(String[] args) {
+toString a1=new toString();
+toString a2=new toString();
+System.out.println(a1);
+}
+public String toString() {
+        return "Hello";
+    }
+}
