c3077e4782c5a4cff202c5d5491c8e98337d53c1
pramodhkesavSoftwareDev <pramodhkesav758@gmail.com>
2021-11-18 14:03:17 +0530

    Array learning

---
+++ Arrays.java
@@ -0,0 +1,11 @@
+public class Arrays {
+ 
+    public static void main(String[] args) {
+     
+        long[] ar=new long[5];
+        int i=0;
+        while(i<ar.length) {
+        System.out.println(ar);
+            System.out.println(ar[i]);
+            i++;
+        }
